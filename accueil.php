e95aa365007a55e3a708fc0513531b89677c4a66
yandetoure <ndeyeyandemouhammad@gmail.com>
2024-02-28 19:17:33 +0000

    creation de la page daccueil git commit -m "creation de la page daccueil
    '
    '
    git add .
    git commit -m 'creation de la connexion à la base de donnee avec le fichier server.php'
    '''

---
+++ accueil.php
@@ -0,0 +1,64 @@
+<?php
+require_once('functions.php');
+require_once('header.php');
+include('server.php');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="accueil.css">
+    <title>Accueuil</title>
+</head>
+
+<body class="body">
+
+    <div class="body-content">
+        <div class="pub">
+                    
+            <h5> <a href="idee.php">Créez votre Idée maintenant</a></h5>
+        </div>
+        <div class="content">
+
+
+            <div class="card-title">
+                <h3>Ndeye Yandé Touré : </h3>
+                <h4> Projet </h4>
+
+            </div>
+            <div class="card-body">
+                Tout ce que tu vois n'est que le début de quelque chose d'encore plus grand, restez sckootché ici pour ne rien manquer de nos articles...
+            </div>
+
+
+            <div class="card-footer">
+                <h5> Mardi, 17h 20</h5>
+                <p>commentaire</p>
+            </div>
+        </div>
+
+        <div class="content">
+            <div class="card-title">
+                <h3>Ndeye Yandé Touré : </h3>
+                <h4> Projet </h4>
+
+            </div>
+            <div class="card-body">
+                Tout ce que tu vois n'est que le début de quelque chose d'encore plus grand, restez sckootché ici pour ne rien manquer de nos articles...
+            </div>
+
+
+            <div class="card-footer">
+                <h5> Mardi, 17h 20</h5>
+                <p>commentaire</p>
+            </div>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
